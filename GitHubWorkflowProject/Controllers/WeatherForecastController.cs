285adb2e14141a9aaf1a13c4fdb09560a2dfd47b
Jere Janhunen <djeja003@edu.xamk.fi>
2024-01-11 08:54:34 +0200

    changed temp values

--- GitHubWorkflowProject/Controllers/WeatherForecastController.cs
+++ GitHubWorkflowProject/Controllers/WeatherForecastController.cs
@@ -19,32 +19,19 @@ namespace GitHubWorkflowProject.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        //public IEnumerable<WeatherForecast> Get()
-        //{
-        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        //        TemperatureC = Random.Shared.Next(-20, 55),
-        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        //    })
-        //    .ToArray();
-        //}
-
         [ProducesResponseType(typeof(string), 200)]
         public IActionResult Get()
         {
             var weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
+                TemperatureC = Random.Shared.Next(-40, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
 
-            // Custom string to be added to the response
             var customString = "Just a test";
 
-            // Returning both the weather forecasts and the custom string
             return Ok(new { WeatherForecasts = weatherForecasts, CustomString = customString });
         }
 
